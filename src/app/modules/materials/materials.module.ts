2eff168925f13d105f2547c93e5880ff3cdd68de
soroushby <49848393+soroushby@users.noreply.github.com>
2021-01-05 04:45:37 -0800

    trying to make toolbar and sidenav

--- src/app/modules/materials/materials.module.ts
+++ src/app/modules/materials/materials.module.ts
@@ -5,6 +5,16 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
   declarations: [],
   imports: [
@@ -13,7 +23,34 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatCardModule,
+    BrowserModule,
+    MatDatepickerModule,
+    MatTableModule,
   ],
-  exports: [MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatSelectModule,
+    MatCardModule,
+    BrowserModule,
+    MatDatepickerModule,
+    MatTableModule,
+  ],
 })
 export class MaterialsModule {}
